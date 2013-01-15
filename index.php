e668bd6494e2b306d2b8bfa5997ba8377ed49fda
dunn007 <dujincheng007@qq.com>
2013-01-15 23:49:06 +0800

    test

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+
+<title>看书网 - 原创小说网站,(最新)小说免费阅读下载 好看的小说阅读网!</title>
+
+<meta name="description" content="看书网,专业原创小说网站,提供最新言情,都市校园,穿越,网游,玄幻,武侠,科幻,历史等小说免费阅读和小说下载,是最好看的小说阅读网。看小说,就上看书网！" />
+
+<script type="text/javascript" src="http://img.kanshu.com/ext/js/jquery-1.4.4.min.js"></script>
+<script type="text/javascript">
+$.getScript("http://my.test2.com");
+alert(k);
+</script>
+</head>
+
+
+
+<body>
+<div id='ccc'></div>
+<?php
+session_start();
+echo session_id();
+?>
+</body>
+</html>
